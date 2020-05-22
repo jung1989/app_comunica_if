8d9632492d8ee0e26fb6e1d6c6f80e79bae6e4f1
jung1989 <mateus_jung@hotmail.com>
2020-05-22 16:38:53 -0300

    Teste branch novo

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class Inicial extends StatefulWidget {
 class _InicialState extends State<Inicial> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column();
   }
 }
 
